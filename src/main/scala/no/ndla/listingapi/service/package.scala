5ec8947c896bc578b77c2463042ca3e15bc860c1
Tanja Gruschke <tmg@knowit.no>
2017-08-03 14:21:37 +0200

    #497 /embed on oembed url

--- src/main/scala/no/ndla/listingapi/service/package.scala
+++ src/main/scala/no/ndla/listingapi/service/package.scala
@@ -4,7 +4,7 @@ package object service {
   def createOembedUrl(oldNodeId: Option[Long]): Option[String] = {
     oldNodeId match {
       case None => None
-      case Some(id) => Option(s"https://ndla.no/node/$id")
+      case Some(id) => Option(s"https://ndla.no/node/$id/embed")
     }
   }
 }
