a10cc6ce6f642a835eae2662d8fdaf3ffd8ade67
Jonas Natten <jonas.natten@knowit.no>
2019-09-03 12:14:00 +0200

    Update src/main/scala/no/ndla/listingapi/model/api/CoverDomainDump.scala
    Co-Authored-By: Gunnar Velle <gunnar.velle@gmail.com>

--- src/main/scala/no/ndla/listingapi/model/api/CoverDomainDump.scala
+++ src/main/scala/no/ndla/listingapi/model/api/CoverDomainDump.scala
@@ -4,7 +4,7 @@ import org.scalatra.swagger.annotations.{ApiModel, ApiModelProperty}
 import scala.annotation.meta.field
 import no.ndla.listingapi.model.domain
 
-@ApiModel(description = "Information about articles")
+@ApiModel(description = "Information about covers")
 case class CoverDomainDump(
     @(ApiModelProperty @field)(description = "The total number of covers in the database") totalCount: Long,
     @(ApiModelProperty @field)(description = "The search results") results: Seq[domain.Cover])
